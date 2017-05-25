20726f52efe4e71b155539fe66ebd5069ee1e43a
eringrand <eringrand@gmail.com>
2017-05-25 16:41:54 -0700

    editing names

--- data-raw/tables.R
+++ data-raw/tables.R
@@ -1,11 +1,11 @@
 library(datapasta)
 
-tables <- c("Arsenal", "ascii", "condformat", "Desctable", 
-  "DT", "expss", "ezsummary", "formattable", 
-  "gmodels", "Gtable", 
-  "Hmisc::latex", "htmlTable", "huxtable", "janitor", "kable",
-"kableExtra", "pander", "pixiedust", "ReporteRs", "stargazer", 
+tables <- c("Arsenal", "ascii", "condformat", "Desctable",
+  "DT", "expss", "ezsummary", "formattable",
+  "gmodels", "Gtable",
+  "Hmisc", "htmlTable", "huxtable", "janitor", "kable",
+"kableExtra", "pander", "pixiedust", "ReporteRs", "stargazer",
 "Tableone", "tables", "tangram", "texreg", "xtable", "Ztable", "Tabular")
 
 
-save(tables, file="data/tables.rdata")
+saveRDS(tables, file="data/tables.rds")
