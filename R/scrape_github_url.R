8cf42d83ef585f0f8b3e756d01390ec2f98f6c38
Sam Firke <samuel.firke@gmail.com>
2017-05-26 15:33:51 -0400

    return github socials as numeric values

--- R/scrape_github_url.R
+++ R/scrape_github_url.R
@@ -74,6 +74,7 @@ get_social_stats_from_html <- function(page_html){
     dplyr::mutate(github_social = stringr::str_extract(`aria-label`, "[[:digit:]]+"),
                   action = c("watchers", "stars", "forks")) %>%
     dplyr::select(action, github_social) %>%
+    dplyr::mutate(github_social = as.numeric(github_social)) %>%
     tidyr::spread(action, github_social)
 
 }
