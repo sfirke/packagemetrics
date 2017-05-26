60d3864db4ef2b3a0234e6eccd9edba8859d7275
bzkrouse <rebecca_zabel@rhoworld.com>
2017-05-26 10:58:58 -0700

    update to search both "bugreports" and "url" for closest match to actual github page

--- R/getGitHub.R
+++ R/getGitHub.R
@@ -1,12 +1,32 @@
 getGitHub <- function(packages){
-  dd <- readRDS('data/cran.rds')
+  load('data/cran.rda')
 
-  out <- dd[ !duplicated(names(dd)) ] %>%
+  cran_urls <- cran %>%
     filter(package %in% packages) %>%
-    mutate(ongithub = str_detect("github.com", bugreports),
-           github = ifelse(is.na(bugreports), NA, stringr::str_replace(bugreports,'/issues',''))) %>%
-    select(package, github, ongithub)
+    select(package, url, bugreports)
 
-  return(out)
+  url <- cran_urls$url[2] #temp
+  find_github <- function(url){
+    url <- gsub("\n", ",", url)
+    url <- gsub(" ", "", url)
+    url <- gsub("https", "http", url)
+    url <- gsub("http", "https", url)
+    url_list <- str_split(url, ",")[[1]]
+    github_url <- url_list[str_detect(url_list, "//github.com")]
+    github_url <- ifelse(length(github_url) == 0, NA, github_url)
+    github_url <- ifelse(str_sub(github_url, -1) == "/", str_sub(github_url, 1, -2), github_url)
+    return(github_url)
+  }
+
+
+  cran_urls %>%
+    mutate(url_git = map_chr(url, find_github),
+           bug_git = map_chr(bugreports, find_github) %>% str_replace("/issues",""),
+           github_url = ifelse(is.na(url_git), bug_git, url_git)
+    ) %>%
+    select(package, github_url) %>%
+    mutate(ongithub = !is.na(github_url))
+
 }
+
 
