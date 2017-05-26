5616f7deaf274e7f1e09650311bba1e29c30a5ad
Hannah Frick <hannah.frick@gmx.de>
2017-05-26 13:26:11 -0700

    added get_hits + comments

--- inst/sandbox/getPkgs.R
+++ inst/sandbox/getPkgs.R
@@ -1,30 +1,55 @@
-library(devtools)
+devtools::install_github("lshep/stackr")
 
-install_github("lshep/stackr")
-
-library(stackr)
-library(tidyr)
+library(stackr) ## -> DESCRIPTION
 library(tidytext)
-library(dplyr)
 
 
+#' Get a list of CRAN packages associated with a topic
+#'
+#' @param topic What are you trying to do?
+#' @param tag StackOverflow tag.
+#' @param pagesize Size of each page to extract (max 100).
+#' @param num_pages Number of pages to extract.
+#' @param repos Vector of repository URLs passed on to \code{available.packages}.
+#' @param ... Passed on to \code{stack_search}.
+#'
+#' @return A vector of packages mentioned in StackOverflow questions on the \code{topic}.
+#' @export
+#' @examples
+#' get_pkgs(topic = "table", tag = "r", pagesize = 100, numpage = 2)
+get_pkgs <- function(topic, tag = "r", pagesize = 100, num_pages = 200,
+                    repos = "https://cran.rstudio.com"){
 
-getPkgs <- function(topic, tag, pagesize, numpage, repos){
+    # get the questions from StackOverflow
+    questions <- stackr::stack_search(tagged = tag, body = topic, filter = "withbody",
+                                      pagesize = pagesize, num_pages = num_pages, ...)
 
-  
-    results <- stack_search(tagged = tag, body = topic, filter="withbody",
-        pagesize=pagesize, num_pages=numpage)
-    
+    # tokenize the body text of the questions
     body <- results[, "body", drop = FALSE]
-    tidyResults <-  body %>% unnest_tokens_(output_col = "word",  input_col = "body",
-                                           token="words")
-    pkgs = as.data.frame(available.packages(repos=repos)[,"Package"])
-    names(pkgs) = "word"
-    
-    mm = match(as.character(pkgs[,1]), as.character(tidyResults[,1]))
+    tidyResults <-  tidytext::unnest_tokens_(body, output_col = "word", input_col = "body",
+                                             token = "words")
+
+    # get the list of packages
+    pkgs <- as.data.frame(available.packages(repos = repos))[,"Package"]
+    names(pkgs) <- "word"
+
+    # match words in question text with available packages
+    mm <- match(as.character(pkgs[,"word"]), as.character(tidyResults[,"word"]))
     as.character(pkgs[which(!is.na(mm)),1])
 
 }
 
 
-#  getPkgs(topic="table", tag="r", pagesize=100, numpage=2, repos="https://cran.rstudio.com")
+
+#' Get number of search results on StackOverflow
+#'
+#' @param pkg Which package are you intersted in?
+#' @param tag StackOverflow tag.
+#' @param pagesize Size of each page to extract (max 100).
+#' @param num_pages Number of pages to extract.
+#' @param ... Passed on to \code{stack_search}.
+get_hits <- function(pkg, tag = "r", pagesize = 100, num_pages = 200, ...){
+    res <- stackr::stack_search(body = pkg, tagged = tag,  pagesize = pagesize,
+                                num_pages = num_pages, ...)
+    nrow(res)
+}
