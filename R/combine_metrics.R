5849c37c0738ccedc1002c24831cd2b10d607c2a
eringrand <eringrand@gmail.com>
2017-05-26 12:59:04 -0700

    add roxygen header information

--- R/combine_metrics.R
+++ R/combine_metrics.R
@@ -1,3 +1,16 @@
+#' @title Combines metrics on a package from CRAN and Github
+#'
+#' @description
+#' Combines metrics on a package from CRAN and Github
+#'
+#' @param package_name name of CRAN package - case-sensitive.
+#'
+#' @return a tibble of package information.
+#' @export
+#' @examples
+#'
+#' combine_metrics("dplyr")
+#'
 combine_metrics <- function(package_name) {
   c_pkg <- cran_metrics(package_name)
   git_pkg <- scrape_github_package_page(package_name)
