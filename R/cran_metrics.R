3d11740b6703cc21d43caedc65b11f2cc2c631e5
eringrand <eringrand@gmail.com>
2017-05-26 11:44:41 -0700

    added package name before all commands

--- R/cran_metrics.R
+++ R/cran_metrics.R
@@ -1,24 +1,21 @@
-library(tidyverse)
-
 count_packages <- function(x){
-  ifelse(!is.na(x), map_int(x, ~str_split(., ", ")[[1]] %>% length(.)), NA)
+  ifelse(!is.na(x), purrr::map_int(x, ~stringr::str_split(., ", ")[[1]] %>% length(.)), NA)
 }
 
 cran_metrics <- function(package_name) {
   cran %>%
-  filter(package %in% package_name) %>%
-  select(package, author,
+  dplyr::filter(package %in% package_name) %>%
+  dplyr::select(package, author,
          imports, depends, suggests, #enhances,
          reverse_imports, reverse_depends, reverse_suggests, reverse_enhances,
-         vignettebuilder, published,
-        title
+         vignettebuilder, published, title
          ) %>%
-  mutate_each(
+  dplyr::mutate_each(
     funs(count = count_packages), depends:reverse_enhances) %>%
-  mutate(
-    author_new = map(author, ~gsub("aut, cre", "", .)),
-    author_count = map(author_new, count_packages), #number of authors
-    tidyverse_happy = ifelse(str_detect(imports, paste(tidyverse::tidyverse_packages(), collapse="|")), 1, 0),
+  dplyr::mutate(
+    author_new = purrr::map(author, ~gsub("aut, cre", "", .)),
+    author_count = purrr::map(author_new, count_packages), #number of authors
+    tidyverse_happy = ifelse(stringr::str_detect(imports, paste(tidyverse::tidyverse_packages(), collapse="|")), 1, 0),
     has_vignette_build = ifelse(is.na(vignettebuilder), 1, 0)
     ) %>%
   select(-author_new, -(imports:reverse_enhances), -vignettebuilder)
