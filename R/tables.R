6eaa3222dda742f756428a9af1d14a5350d54a50
eringrand <eringrand@gmail.com>
2017-05-26 15:12:31 -0700

    chaning spaces for cleaner look

--- R/tables.R
+++ R/tables.R
@@ -1,13 +1,11 @@
 # List of table packages
+tables <- c("arsenal", "ascii", "condformat", "desctable",
+            "DT", "expss", "ezsummary", "formattable",
+            "gmodels", "gtable",
+            "Hmisc", "htmlTable", "huxtable", "janitor", "knitr",
+            "kableExtra", "pander", "pixiedust", "ReporteRs", "stargazer",
+            "tableone", "tables", "tangram", "texreg", "xtable", "ztable", "compareGroups")
 
-
-  tables <- c("arsenal", "ascii", "condformat", "desctable",
-              "DT", "expss", "ezsummary", "formattable",
-              "gmodels", "gtable",
-              "Hmisc", "htmlTable", "huxtable", "janitor", "knitr",
-              "kableExtra", "pander", "pixiedust", "ReporteRs", "stargazer",
-              "tableone", "tables", "tangram", "texreg", "xtable", "ztable", "compareGroups")
-
-  devtools::use_data(tables, overwrite = TRUE) # for interactive use
-  #devtools::use_data(tables, internal = TRUE) # for internal functions
+devtools::use_data(tables, overwrite = TRUE) # for interactive use
+#devtools::use_data(tables, internal = TRUE) # for internal functions
 
