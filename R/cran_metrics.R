cbd402ff08eef8c780e1906b68eae8c7d53ccc07
eringrand <eringrand@gmail.com>
2017-05-26 14:26:20 -0700

    small changes to pruning

--- R/cran_metrics.R
+++ R/cran_metrics.R
@@ -13,25 +13,26 @@
 
 cran_metrics <- function(package_name) {
   tv_packages <- c("broom",     "dplyr",     "forcats",   "ggplot2",   "haven",     "httr",      "hms",
-                          "jsonlite",  "lubridate", "magrittr",  "modelr",    "purrr",     "readr",     "readxl",
-                          "stringr",   "tibble",    "rvest",     "tidyr",     "xml2",      "tidyverse")
+                   "jsonlite",  "lubridate", "magrittr",  "modelr",    "purrr",     "readr",     "readxl",
+                   "stringr",   "tibble",    "rvest",     "tidyr",     "xml2",      "tidyverse")
   cran %>%
   dplyr::filter(package %in% package_name) %>%
-  dplyr::select(package, author,
-         imports, depends, suggests, #enhances,
-         reverse_imports, reverse_depends, reverse_suggests, reverse_enhances,
+  dplyr::select(package,
+         imports, depends, suggests,
+         reverse_imports, reverse_depends,
          vignettebuilder, published, title
          ) %>%
   dplyr::mutate_each(
-    dplyr::funs(count = count_packages), depends:reverse_enhances) %>%
+    dplyr::funs(count = count_packages), depends:reverse_depends) %>%
   dplyr::mutate(
-    author_new = purrr::map(author, ~gsub("aut, cre", "", .)),
-    author_count = purrr::map(author_new, count_packages), #number of authors
-    tidyverse_happy = ifelse(stringr::str_detect(imports, paste(tv_packages, collapse="|")), 1, 0),
-    has_vignette_build = ifelse(is.na(vignettebuilder), 1, 0)
+    tidyverse_happy = ifelse(stringr::str_detect(imports, paste(tv_packages, collapse="|")), TRUE, FALSE),
+    has_vignette_build = ifelse(is.na(vignettebuilder), TRUE, FALSE),
+    has_tests = ifelse(str_detect(suggests, "testthat|RUnit"), TRUE, FALSE),
+    has_tests = ifelse(is.na(has_tests), FALSE, has_tests),
+    reverse_count = reverse_imports_count + reverse_depends_count
     ) %>%
-  dplyr::select(-author_new, -(imports:reverse_enhances), -vignettebuilder) %>%
-  dplyr::left_join(get_cran_downloads(package_name), by="package")
+  dplyr::select(-(imports:reverse_depends), -vignettebuilder, -reverse_imports_count, -reverse_depends_count) %>%
+  cbind(get_cran_downloads(package_name))
 }
 
 count_packages <- function(x){
@@ -40,13 +41,8 @@ count_packages <- function(x){
 
 
 get_cran_downloads <- function(package_name){
-  cran_dl_last_day <- cranlogs::cran_downloads(packages = package_name, when = "last-day") %>%
-    dplyr::rename(dl_last_day = count) %>%
-    select(package, dl_last_day)
-  cran_dl_last_week <- cranlogs::cran_downloads(packages = package_name, when = "last-week") %>%
-    dplyr::summarise(dl_last_week = sum(count))
-  cran_dl_last_month <- cranlogs::cran_downloads(packages = package_name, when = "last-month") %>%
+  cranlogs::cran_downloads(packages = package_name, when = "last-month") %>%
     dplyr::summarise(dl_last_month = sum(count))
-  dplyr::bind_cols(cran_dl_last_day, cran_dl_last_week, cran_dl_last_month)
 }
+
 
