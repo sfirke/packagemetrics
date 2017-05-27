c9be71013837f479e01ad6c05ad5d1fa963ac1d2
Lori Shepherd <shepherl@gmail.com>
2017-05-26 20:19:55 -0400

    Document data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Cran data
+#'
+#' @format A dataframe of CRAN metadata
+"cran"
+
+#' Table of Packages
+#'
+#' @format A character vector of packages
+"table_packages"
+
+#' Table of Packages
+#'
+#'  @format A character vector of packages
+"tables"
