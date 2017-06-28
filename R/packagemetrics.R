c84d76adcefd79a672ef313683c01fb2a8a7d6e5
Colin Gillespie <csgillespie@gmail.com>
2017-06-28 10:45:30 +0100

    Remove R version check
    The Description file states that R >= 3.4, therefore the version check is redundant.

--- R/packagemetrics.R
+++ R/packagemetrics.R
@@ -8,4 +8,4 @@
 #' @keywords internal
 NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+utils::globalVariables(c("."))
