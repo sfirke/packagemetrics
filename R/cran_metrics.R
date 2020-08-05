96163782b4e94ff32aa1ec34afbc6e32c7ad46e4
Sam Firke <sfirke@users.noreply.github.com>
2020-08-05 11:24:07 -0400

    replace funs() with across()
    Should close #50

--- R/cran_metrics.R
+++ R/cran_metrics.R
@@ -32,7 +32,7 @@ cran_metrics <- function(package_name, forget = FALSE) {
          reverse_imports, reverse_depends,
          vignettebuilder, published, title
          ) %>%
-  dplyr::mutate_at(dplyr::vars(depends:reverse_depends), dplyr::funs(count = count_packages)) %>%
+  dplyr::mutate(dplyr::across(depends:reverse_depends, count_packages)) %>%
   dplyr::mutate(
     tidyverse_happy = ifelse(stringr::str_detect(imports, paste(tv_packages, collapse = "|")), TRUE, FALSE),
     has_vignette_build = ifelse(is.na(vignettebuilder), FALSE, TRUE),
